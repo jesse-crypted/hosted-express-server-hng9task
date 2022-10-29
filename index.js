62ba16a8a1a08f9cce5f674da612453097154e03
Jesse <jesseolatunjioluwadare@gmail.com>
2022-10-29 19:04:22 +0100

    rename

---
+++ index.js
@@ -0,0 +1,25 @@
+const express = require("express");
+const cors = require("cors");
+
+const app = express();
+
+app.use(
+  cors({
+    origin: "*",
+  })
+);
+
+app.get("/", (req, res) => {
+  res.status(200).json({
+    slackUsername: "jessecrack",
+    backend: true,
+    age: 19,
+    bio: "Hi 👋, I'm a backend dev who want to build digital experiences for the local church",
+  });
+});
+
+const port = 4040;
+
+app.listen(port, () => {
+  console.log(`server running on port ${port}`);
+});
